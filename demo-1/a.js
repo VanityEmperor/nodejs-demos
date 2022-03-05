4ab74658644ae38c4c94f78b7e513e13c247b20a
姬虚空 <2765812425@qq.com>
2022-03-05 11:40:47 +0800

    导出对象

--- demo-1/a.js
+++ demo-1/a.js
@@ -1,4 +1,10 @@
 function add(a,b){
     return a + b
 }
-module.exports = add
+function mul(a,b){
+    return a * b
+}
+module.exports = {
+    add,
+    mul
+}
--- demo-1/b.js
+++ demo-1/b.js
@@ -1,3 +1,5 @@
-const add = require('./a')
+const {add,mul} = require('./a')
 const sum = add(10,20)
+const result = mul(100,200)
 console.log(sum)
+console.log(result)
