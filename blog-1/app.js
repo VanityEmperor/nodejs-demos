f7c31c2d8e09f5b83858f21fee872fde9a78870c
姬虚空 <2765812425@qq.com>
2022-03-09 21:22:27 +0800

    初始化路由

--- blog-1/app.js
+++ blog-1/app.js
@@ -1,14 +1,34 @@
+const handleBlogRouter = require('./src/router/blog')
+const handleUserRouter = require('./src/router/user')
 const serverHandle = (req,res) =>{
     // 设置返回格式 JSON
     res.setHeader('Content-type','application/json')
 
-    const resData = {
-        name: '虚空大帝',
-        site: 'jixukong',
-        env: process.env.NODE_ENV
+    // 获取 path
+    const url = req.url
+    req.path = url.split('?')[0]
+    // 处理 blog 路由
+    const blogData = handleBlogRouter(req,res)
+    if(blogData){
+        res.end(
+            JSON.stringify(blogData)
+        )
+        return
     }
 
-    res.end(JSON.stringify(resData))
+    // 处理 user 路由
+    const userData = handleUserRouter(req,res)
+    if(userData){
+        res.end(
+            JSON.stringify(userData)
+        )
+        return
+    }   
+    
+    // 未命中路由，返回 404
+    res.writeHead(404,{"Content-type":"text/plain"})
+    res.write("404 Not Found")
+    res.end()
 }
 
 module.exports = serverHandle
