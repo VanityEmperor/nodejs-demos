76da8fccc3cd4b734e0bbb5ce5af9ebd1dea0c0f
姬虚空 <2765812425@qq.com>
2022-03-15 22:18:00 +0800

    mysql博客列表

--- blog-1/src/controller/blog.js
+++ blog-1/src/controller/blog.js
@@ -1,23 +1,18 @@
+const { exec } = require('../db/mysql')
 const getList = (author,keyword)=>{
-    // 先返回假数据（格式是正确的）
-    return [
-        {
-            id:1,
-            title:'标题A',
-            content:'内容A',
-            createTime:1646833060625,
-            author:'zhangsan'
-        },
-        {
-            id:2,
-            title:'标题B',
-            content:'内容B',
-            createTime:1646833100232,
-            author:'lisi'
-        },
+    let sql = `select * from blogs where 1=1 `
+    if (author){
+        sql += `and author='${author}' `
+    }
+    if (keyword){
+        sql += `and title like '%${keyword}%' `
+    }
+    sql += `order by createtime desc;`
 
-    ]
+    // 返回 promise
+    return exec(sql)
 }
+
 
 const getDatail = (id)=>{
     // 先返回假数据
