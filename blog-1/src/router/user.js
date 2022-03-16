6aaaad6a7305eaeda36837f2c050b3c7330e3182
姬虚空 <2765812425@qq.com>
2022-03-16 22:41:37 +0800

    cookie时间限制

--- blog-1/src/router/user.js
+++ blog-1/src/router/user.js
@@ -1,5 +1,12 @@
 const {login} = require('../controller/user')
 const {SuccessModel,ErrorModel} = require('../model/resModel')
+
+// 获取 cookie 的过期时间
+const getCookieExpires = ()=>{
+    const d = new Date()
+    d.setTime(d.getTime()+(24*60*60*1000))
+    return d.toGMTString()
+}
 const handleUserRouter = (req,res) =>{
     const method = req.method // GET POST
 
@@ -17,7 +24,7 @@ const handleUserRouter = (req,res) =>{
         return result.then(data =>{
             if(data.username){
                 //操作cookie
-                res.setHeader('Set-Cookie',`username=${data.username};path=/; httpOnly`)
+                res.setHeader('Set-Cookie',`username=${data.username};path=/; httpOnly; expires=${getCookieExpires()}`)
                 return new SuccessModel()
             }
             return new ErrorModel('登录失败')
