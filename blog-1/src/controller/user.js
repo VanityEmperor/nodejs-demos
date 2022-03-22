65666ffa3437b55a7c5b702f1da1968798293013
姬虚空 <2765812425@qq.com>
2022-03-22 22:22:58 +0800

    “sql注入”

--- blog-1/src/controller/user.js
+++ blog-1/src/controller/user.js
@@ -1,8 +1,12 @@
-const { exec } = require('../db/mysql')
+const { exec,escape } = require('../db/mysql')
 const login = (username,password)=>{
+    username = escape(username)
+    password = escape(password)
     let sql = `
-        select username,password from users where username='${username}' and password='${password}'
+        select username,password from users where username=${username} and password=${password}
     `
+    // select username,realname from users where username='zhangsan' -- ' and password='123'
+    console.log('sql',sql)
     return exec(sql).then(rows =>{
         return rows[0] || {}
     })
